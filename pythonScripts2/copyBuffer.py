8101ebb67093eaf112030e2e21e4177d092cb99f
eslammostafa1 <eslameslammostafa@gmail.com>
2023-08-30 12:07:29 +0200

    copy buffer

---
+++ pythonScripts2/copyBuffer.py
@@ -0,0 +1,8 @@
+# import copy
+import pyperclip
+
+# Get the data from the clipboard
+pyperclip_data = pyperclip.paste()
+
+# Print the contents of the clipboard
+print("Clipboard Data:", pyperclip_data)
