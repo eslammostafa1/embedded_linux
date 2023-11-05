9bf87e9296062020b865cf010ba3dc4419c13a44
eslammostafa1 <eslameslammostafa@gmail.com>
2023-11-05 22:16:30 +0200

    string

---
+++ test/string.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::string str = "my name is eslam";
+  int length = str.size();  
+  std::cout << "The length of the string is: " << length << std::endl;
+    return 0;
+}
