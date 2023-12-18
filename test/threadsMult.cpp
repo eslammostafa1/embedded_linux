7c8eb9e95f7206f5bb14a7af3d7de52c223060e2
eslammostafa1 <eslameslammostafa@gmail.com>
2023-12-18 21:08:18 +0200

    mutex

--- test/threadsMult.cpp
+++ test/threadsMult.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
 
 
 auto lambda=[](int x){
     std::cout << " thread id with lambda :" << std::this_thread::get_id() << '\n';
     std::cout << " passed argument : " << x << '\n';
 };
+
+// mutex synchronization primitive that can be used to protect shared data from being simultaneously accessed by multiple threads.
+std::mutex glock;
+static int shared_val = 0;
+void shared_val_increment(){
+    std::lock_guard<std::mutex> gGuard(glock);
+        shared_val = shared_val +1;
+        
+    // repalce lock and unlock with lock_guard 
+    // glock.lock();
+    // try{
+    //     shared_val = shared_val +1;
+    //     throw " aborted ....";
+    // }
+    // catch(...){
+    //     std::cout << "handle exception" << '\n';
+    //     glock.unlock();
+    // return;
+    // }
+    // glock.unlock();
+}
 
 int main() {
     std::cout << "----- my main thread --------" << '\n';
@@ -18,14 +40,14 @@ int main() {
     //     threadvec[i].join();
     // }
 
-    // this execute 10 threads and 
-    std::vector<std::thread> threadvec2;
-    for (int i=0; i <10; i++) {
-        threadvec2.push_back(std::thread(lambda, i));
-    }
-    for (int i=0; i <10; i++) {
-        threadvec2[i].join();
-    }
+    // // this execute 10 threads and 
+    // std::vector<std::thread> threadvec2;
+    // for (int i=0; i <10; i++) {
+    //     threadvec2.push_back(std::thread(lambda, i));
+    // }
+    // for (int i=0; i <10; i++) {
+    //     threadvec2[i].join();
+    // }
         
     // // this execute 10 threads by using jthread it join by its destrcutor c++20 
     // std::vector<std::jthread> jthreadvec2;
@@ -33,6 +55,15 @@ int main() {
     //     jthreadvec2.push_back(std::jthread(lambda, i));
     // }
 
-        
+    //mutex
+    // if  i run this code many times the threads do not finish it will give me 999 or 997 so we use mutex 
+    std::vector<std::thread> threadvec3;
+    for (int i=0; i <10000; i++) {
+        threadvec3.push_back(std::thread(shared_val_increment));
+    }
+    for (int i=0; i <10000; i++) {
+        threadvec3[i].join();
+    }
+    std::cout << "shared value : " << shared_val << '\n';
     return 0;
 }
