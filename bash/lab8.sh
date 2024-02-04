cd45a17a4bb3ccf55ce405ed441ce4a9c6a3f827
eslammostafa1 <eslameslammostafa@gmail.com>
2024-02-04 19:12:12 +0200

    bash7,8up

--- bash/lab8.sh
+++ bash/lab8.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-main_file="system_info.txt" 
+main_file="system_info.txt" >"$main_file"
 
->"$main_file"
-
-add_section(){
-    echo "--------------------------" >>"$main_file" 
-    echo "$1" >> "$main_file"
-    echo "--------------------------" >>"$main_file" 
-    
-}
+add_section()
+{
+    echo "--------------------------" 
+    echo "$1" 
+    echo "--------------------------"    
+} >>"$main_file" 
 
 add_section "system report - $(date)"
-hostname >> "$main_file"
-whoami >> "$main_file"
-echo -e "\n" >> "$main_file"
+{
+    hostname 
+    echo -e "\n" 
+} >> "$main_file"
+
 
 add_section "CPU info "
-lscpu >> "$main_file"
-echo -e "\n" >> "$main_file"
+{
+    lscpu 
+    echo -e "\n" 
+} >> "$main_file"
+
 
 add_section "memory info "
-free >> "$main_file"
-echo -e "\n" >> "$main_file"
+{
+    free -h
+    echo -e "\n" 
+} >> "$main_file"
+
 
 add_section "disk info "
-df -h >> "$main_file"
-echo -e "\n" >> "$main_file"
+{
+    df -h 
+    echo -e "\n" 
+} >> "$main_file"
+
 
 add_section "network info "
-ifconfig >> "$main_file"
-echo -e "\n" >> "$main_file"
+{
+    ifconfig 
+    echo -e "\n" 
+} >> "$main_file"
